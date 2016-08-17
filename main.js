67e39e30f137cd3eec7de461fea2c4a0306b5be8
qsctr <brettonchen@gmail.com>
2016-08-17 18:00:35 +0800

    Add blog thing

---
+++ main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+Array.prototype.slice.call(document.querySelector('#projects').children).forEach(function (project) {
+    project.addEventListener('click', function () {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', '/posts/' + project.textContent + '.md?_=' + new Date().getTime());
+        xhr.onload = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                console.log(xhr.responseText);
+                document.querySelector('#post').innerHTML = marked(xhr.responseText);
+            }
+        };
+        xhr.onerror = function (e) {
+            console.error(xhr.statusText);
+        };
+        xhr.send(null);
+    });
+});
